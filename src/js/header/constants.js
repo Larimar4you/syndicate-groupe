c3ef5489cf0882c90f0acc4975fe537ed0491a1c
Erik K. <erickopcha@gmail.com>
2026-03-25 21:05:47 +0100

    refactor(header): introduce shared constants module

---
+++ src/js/header/constants.js
@@ -0,0 +1,16 @@
+export const ACTIVE_NAV_LINK_CLASS = 'is-active';
+export const OPEN_CLASS = 'is-open';
+export const SCROLLED_CLASS = 'is-scrolled';
+export const HEADER_HEIGHT_VARIABLE = '--header-height';
+
+export const HEADER_SELECTORS = Object.freeze({
+  headerRoot: '[data-site-header]',
+  navLink: '[data-nav-link][href^="#"]',
+  backdrop: '[data-menu]',
+  menuOpenButton: '[data-menu-open]',
+  menuCloseButton: '[data-menu-close]',
+  languageDropdown: '[data-language-dropdown]',
+  languageTrigger: '[data-language-trigger]',
+  languageOption: '[data-language-option][data-language]',
+  languageCurrent: '[data-language-current]',
+});
