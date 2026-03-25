f50a8e04b6db7310c149a51a7e0db12c573c6594
Erik K. <erickopcha@gmail.com>
2026-03-25 21:07:03 +0100

    refactor(app): wire new header module and remove legacy api

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,12 @@
 import { Carousel } from './carousel/index.js';
+import { initHeader } from './header/index.js';
 import { createI18nService } from './i18n/bootstrap.js';
-import './mobile-menu.js';
 
 const i18nService = createI18nService();
 const activeLanguage = i18nService.setLanguage(
   i18nService.resolveInitialLanguage()
 );
 
-/**
- * Temporary global API for future header language switcher integration.
- * Will be replaced with dedicated UI controls in header implementation.
- */
-window.appI18n = Object.freeze({
-  setLanguage: language => i18nService.setLanguage(language),
-  getLanguage: () => document.documentElement.lang,
-});
-
 document.documentElement.dataset.appLanguage = activeLanguage;
+initHeader({ i18nService });
 Carousel.initAll();
